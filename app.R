f652d52eb64b8267cda25d0f2e20c16e192bd03c
Diego Ribeiro <drchaves@inf.ufsm.br>
2024-07-07 18:01:05 -0300

    :zap: perf: Estado do mapa salvo apenas ao mexer no slider

--- app.R
+++ app.R
@@ -39,21 +39,36 @@ server <- function(input, output, session) {
   # Variável reativa para armazenar o estado do mapa
   estado_mapa <- reactiveValues(lng = -53.0, lat = -29.0, zoom = 6)
   
-  observe({
-    invalidateLater(1000, session)
-    if (!is.null(input$mapa_center)) {
-      estado_mapa$lng <- input$mapa_center$lng
-      estado_mapa$lat <- input$mapa_center$lat
-      estado_mapa$zoom <- input$mapa_zoom
-    }
-  })
-  
   output$mapa <- renderLeaflet({
     subset_dados <- subset(dados, mes == input$mes)
     raios_normalizados <- normaliza_raio(subset_dados[["diferenca"]])
     
     leaflet() %>%
       addTiles() %>%
+      setView(lng = estado_mapa$lng, lat = estado_mapa$lat, zoom = estado_mapa$zoom) %>%
+      addCircleMarkers(
+        lng = subset_dados[["longitude"]],
+        lat = subset_dados[["latitude"]],
+        radius = raios_normalizados,
+        popup = paste("Cidade:", subset_dados[["municipio"]], "<br>",
+                      "Internações no mês:", subset_dados[["diferenca"]], "<br>",
+                      "A cada 1000 habitantes: ", subset_dados[["internacoes_por_1000"]])
+      )
+  })
+  
+  observeEvent(input$mes, {
+    # Atualizar o estado do mapa
+    if (!is.null(input$mapa_center)) {
+      estado_mapa$lng <- input$mapa_center$lng
+      estado_mapa$lat <- input$mapa_center$lat
+      estado_mapa$zoom <- input$mapa_zoom
+    }
+    
+    subset_dados <- subset(dados, mes == input$mes)
+    raios_normalizados <- normaliza_raio(subset_dados[["diferenca"]])
+    
+    leafletProxy("mapa", session) %>%
+      clearMarkers() %>%
       setView(lng = estado_mapa$lng, lat = estado_mapa$lat, zoom = estado_mapa$zoom) %>%
       addCircleMarkers(
         lng = subset_dados[["longitude"]],
